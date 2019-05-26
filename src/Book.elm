7f2b416bbbc361d8fae252d8a8ea377fe6b78f28
= <tysonbattistella@gmail.com>
2019-05-25 22:16:01 -0700

    writing down the bones

--- src/Book.elm
+++ src/Book.elm
@@ -15,7 +15,8 @@ type alias Book =
 
 books : List Book
 books =
-    [ Book "Stubborn Attachments: A Vision for a Society of Free, Prosperous, and Responsible Individuals" "Tyler Cowen" 2019 8 "https://amzn.to/2UBqeht"
+    [ Book "Writing Down the Bones: Freeing the Writer Within" "Natalie Goldberg" 2019 8 "https://amzn.to/2MaDLc8"
+    , Book "Stubborn Attachments: A Vision for a Society of Free, Prosperous, and Responsible Individuals" "Tyler Cowen" 2019 8 "https://amzn.to/2UBqeht"
     , Book "Norwegian Wood" "Haruki Murakami" 2019 9 "https://amzn.to/2OP7gxH"
     , Book "Hackers & Painters: Big Ideas from the Computer Age" "Paul Graham" 2019 8 "https://amzn.to/2Tv3wC9"
     , Book "Digital Minimalism: Choosing a Focused Life in a Noisy World" "Cal Newport" 2019 10 "https://amzn.to/2EseoLR"
